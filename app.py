77c140bdcd380e86f1f5e5da8dba43bd1e494c43
Zhiheng Chen <czhdawang@163.com>
2024-05-01 17:29:38 +1000

    add functions for getting details

--- app.py
+++ app.py
@@ -5,20 +5,28 @@ from flask import request
 from cassandra.cluster import Cluster
 import requests
 from bs4 import BeautifulSoup
+from flask_cors import CORS, cross_origin
 
 cluster = Cluster(['192.168.0.1', '192.168.0.2'])
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
 # cluster.connect()
 
 def get_url_base():
     return "https://www.themoviedb.org"
 
+
 def get_search_url(keyword, language):
-    return get_url_base() +"/search?query="+ keyword +"&language=" + language
+    return get_url_base() + "/search?query=" + keyword + "&language=" + language
+
 
 def get_detail_url(url):
     return get_url_base() + url
+
 
 def save_to_database(metadata):
     pass
@@ -28,7 +36,8 @@ def resolveMeta(file, job):
     soup = BeautifulSoup(file, "html.parser")
     return job(soup)
 
-def requestDispatcher(method,request_url,header=None):
+
+def requestDispatcher(method, request_url, header=None):
     if header is None:
         header = {
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
@@ -50,7 +59,8 @@ def requestDispatcher(method,request_url,header=None):
     return requests.request(method, request_url, headers=header)
 
 
-@app.route('/movie/get_meta')
+@app.route('/movie/get_meta', methods=['GET'])
+@cross_origin()
 def get_meta():  # get name of a movie.
     # print(request.args.get("detail_address"))
     # print(get_detail_url(request.args.get("detail_address")))
@@ -71,12 +81,27 @@ def get_meta():  # get name of a movie.
         tag = body.find("h3", {"class": "tagline"})
         if tag is not None:
             tag = tag.text
+
+        level = body.find("span", {"class": "certification"})
+        if level is not None:
+            level = level.text
+
+        movie_name = body.find("h2", {"class": "2"})
+        print(movie_name)
+        release_year = body.find("span",{"class":"tag release_date"})
+
+        if release_year is not None:
+            release_year = release_year.text
+
+        if movie_name is not None and movie_name.a is not None:
+            movie_name = movie_name.a.text
+
         actress = body.find("ol", {"class": "people scroller"})
 
         actressList = []
 
         if actress is not None:
-            actresses = actress.find_all("li",{"class" : "card"})
+            actresses = actress.find_all("li", {"class": "card"})
             for actress in actresses:
                 actorDetail = None
                 name = None
@@ -90,31 +115,57 @@ def get_meta():  # get name of a movie.
                 if character is not None:
                     character = character.text
 
-                actressList.append({"actorDetailPage": actorDetail,"character": character, "avatar": avatar, "name": name})
+                actressList.append(
+                    {"actorDetailPage": actorDetail, "character": character, "avatar": avatar, "name": name})
 
-        pictures = body.find_all("div", {"class": "backdrop glyphicons_v2 picture grey no_image_holder no_border no_border_radius"})
+        pictures = body.find_all("div", {
+            "class": "backdrop glyphicons_v2 picture grey no_image_holder no_border no_border_radius"})
         picturesList = []
         for picture in pictures:
             if picture.img is not None:
                 picturesList.append(picture.img["src"])
 
+        makers = body.find_all("li", {"class": "profile"})
+        makersList = []
+        if makers is not None:
+            for maker in makers:
+                info = maker.find_all("p")
+                if len(info) == 2:
+                    makersList.append({"name": info[0].a.text, "role":info[1].text})
+
+        genresList = []
+        genre = body.find("span", {"class" : "genres"})
+        if genre is not None:
+            genres = genre.find_all("a")
+            if genres is not None:
+                for genre in genres:
+                    if genre.text:
+                        genresList.append(genre.text)
         return_result["poster"] = poster
         return_result["score"] = score
         return_result["introduction"] = introduction
         return_result["tag"] = tag
+        return_result["movie_name"] = movie_name
+        return_result["release_year"] = release_year
+        return_result["level"] = level
         return_result["actressList"] = actressList
         return_result["pictureList"] = picturesList
+        return_result["makerList"] = makersList
+        return_result["genre_list"] = genresList
 
         return json.dumps(return_result, ensure_ascii=False)
+
     return resolveMeta(result.text, handler)
 
 
-@app.route("/movie/search")
+@app.route("/movie/search", methods=['GET'])
+@cross_origin()
 def searchMovies():
     keyword = request.args.get("keyword")
     page_number = request.args.get("page_number")
     print(get_search_url(keyword, "zh-CN"))
     result = requestDispatcher("get", get_search_url(keyword, "zh-CN"))
+
     def handler(result):
         return_result = []
         for movie in result.body.div.main.section.div.div.div.next_sibling.next_sibling.section.div.div.children:
@@ -123,15 +174,14 @@ def searchMovies():
 
             attributes = []
             for detail in movie.div.children:
-                 if (detail is None) or (len(str(detail).strip()) == 0):
-                     continue
-                 else:
-                     attributes.append(detail)
-
-
+                if (detail is None) or (len(str(detail).strip()) == 0):
+                    continue
+                else:
+                    attributes.append(detail)
             img_address = attributes[0].find("img")
             detail_address = attributes[1].find("a", {"class": "result"})
-            translated_name = attributes[1].div.div.a.h2.next_element
+            translated_name = attributes[1].find("h2")
+
             original_name = attributes[1].find("span", {"class": "title"})
             release_date = attributes[1].find("span", {"class": "release_date"})
             introduction = attributes[1].find("p")
@@ -145,15 +195,19 @@ def searchMovies():
                 img_address = img_address["src"]
             if original_name is not None:
                 original_name = original_name.text
+            if translated_name is not None:
+                translated_name = translated_name.next
             # if img_address is not None:
             #     response = requests.get(img_address)
             #     print(response)
 
             # print(movie)
-            return_result.append({"image_address": img_address, "translated_name": translated_name, "original_name":original_name, "release_date" : release_date,"introduction":introduction, "detail_address":detail_address})
+            return_result.append(
+                {"image_address": img_address, "translated_name": translated_name, "original_name": original_name,
+                 "release_date": release_date, "introduction": introduction, "detail_address": detail_address})
         return json.dumps(return_result, ensure_ascii=False)
-    return resolveMeta(result.text, handler)
 
+    return resolveMeta(result.text, handler)
 
 
 @app.route('/movie/get_suggestions')
