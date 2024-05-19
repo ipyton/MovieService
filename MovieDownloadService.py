d8b99a4fd1330d86608199620bb5b8b114703ded
Zhiheng Chen <czhdawang@163.com>
2024-05-20 00:22:35 +1000

    add functions.

--- MovieDownloadService.py
+++ MovieDownloadService.py
@@ -11,42 +11,77 @@ cluster = Cluster(['192.168.0.1', '192.168.0.2'])
 app = Flask(__name__)
 cors = CORS(app, resources={r"*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
-
+aria2 = aria2p.API(
+    aria2p.Client(
+        host="http://localhost",
+        port=6800,
+        secret=""
+    )
+)
 
 instance = cluster.connect()
 
 @app.route('/movie/add_source', methods=['POST'])
 @cross_origin()
-def add_event():
+def add_source():
     movieId = request.args.get("movieId")
     source = request.args.get("source")
     instance.execute("insert into movie.resource (movieId, resource, status) values (?,?,?)", movieId, source, "init")
+    aria2.get_downloads()
     return "success"
 
 
-@app.route('/movie/modify_event', methods=['POST'])
+
+@app.route('/movie/start', methods=['POST'])
 @cross_origin()
-def modify_event():
-    action = request.args.get("action")
+def download():
+    movieId = request.args.get("movieId")
     source =request.args.get("source")
-    if action == "delete":
+    instance.execute("insert into movie.resource (movieId, resource, status) values (?,?,?)", movieId, source,
+                     "downloading")
+    if source.split(":") == "magnet":
+        aria2.add_magnet(source)
+    else:
+        aria2.add_uris([source])
 
+@app.route('/movie/pause', methods=['POST'])
+@cross_origin()
+def pause():
+    movieId = request.args.get("movieId")
+    gid = request.args.get("gid")
+    source =request.args.get("source")
+    instance.execute("insert into movie.resource (movieId, resource, status) values (?,?,?)", movieId, source,
+                     "canceled")
+    download = aria2.get_download(gid)
+    aria2.pause(download)
 
-        pass
-    elif action == "pause":
+@app.route('/movie/resume', methods=['POST'])
+@cross_origin()
+def resume():
+    movieId = request.args.get("movieId")
+    gid = request.args.get("gid")
+    source =request.args.get("source")
+    instance.execute("insert into movie.resource (movieId, resource, status) values (?,?,?)", movieId, source,
+                     "downloading")
+    download = aria2.get_download(gid)
+    aria2.resume(download)
 
-
-        pass
-    elif action == "start":
-
-
-        pass
+def resume():
+    movieId = request.args.get("movieId")
+    gid = request.args.get("gid")
+    source = request.args.get("source")
+    instance.execute("insert into movie.resource (movieId, resource, status) values (?,?,?)", movieId, source,
+                     "canceled")
+    download = aria2.get_download(gid)
+    aria2.remove(download)
 
 
 @app.route('/movie/get_download_status', methods=['GET'])
 @cross_origin()
 def get_download_status():
-    print(request.args.get("keyword"))
+    downloads = aria2.get_downloads()
+    for download in downloads:
+        print(download.name, download.download_speed)
     return "download status"
 
 
