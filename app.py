a8013098dcc2fbdf18b919660c54558b19dadce4
ipyton <czhdawang@163.com>
2025-05-17 22:53:52 +1000

    trigger

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ if __name__ == "__main__":
         logging.info("Starting Flask application")
         app.run(host="0.0.0.0", port=8081)
 
+
     except Exception as e:
         logging.critical(f"Main process error: {e}")
         traceback.print_exc()
