22bf309de4800c40b1573f24e80402a3bdd3274a
ipyton <56996660+ipyton@users.noreply.github.com>
2025-07-02 12:32:24 +1000

    redeploy

--- FileExtractor.py
+++ FileExtractor.py
@@ -14,7 +14,6 @@ import os
 from openai import OpenAI
 
 client = OpenAI(
-    # 若没有配置环境变量，请用百炼API Key将下行替换为：api_key="sk-xxx",
     api_key=os.getenv("DASHSCOPE_API_KEY"),  # 如何获取API Key：https://help.aliyun.com/zh/model-studio/developer-reference/get-api-key
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1"
 )
